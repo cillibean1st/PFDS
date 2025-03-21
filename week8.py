12069f69d2fb68121ad76e98bd4fa010cc444c1e
Andrew Pepper <117658946+cillibeanThe1st@users.noreply.github.com>
2025-03-21 10:32:53 +0000

    importing files

---
+++ week8.py
@@ -0,0 +1,13 @@
+#importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+# read the datasets
+dataset = pd.read_csv("../input/weight-height.csv")
+
+# check the dataset
+dataset.head()
+
+
