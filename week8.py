0233d17a68c10c9c1776f5879f30535bcac48a89
Andrew Pepper <117658946+cillibeanThe1st@users.noreply.github.com>
2025-03-21 11:13:46 +0000

    machine learning

--- week8.py
+++ week8.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 # read the datasets
 dataset = pd.read_csv("weight-height.csv")
@@ -35,4 +37,31 @@ plt.ylabel("Weight")
 plt.title("Height vs Weight")
 plt.legend(loc='lower right')
 plt.show()
+
+# separating the dependent and independent values
+# X-independent variable
+X = dataset.iloc[:, 1:2].values
+print(X)
+
+# y-dependent on target
+y = dataset.iloc[:, 2].values
+print(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# creating linear regression model
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# predicting the test set
+y_pred = regressor.predict(X_test)
+
+
+print(y_pred)
+print('Coefficients: ', regressor.coef_)
+# the mean squared error
+print("Mean squared error: %.2f" % np.mean((regressor.predict(X_test) - y_test)**2))
+# explained variance score
+print('Variance score: %.2f' % regressor.score(X_test, y_test))
 
