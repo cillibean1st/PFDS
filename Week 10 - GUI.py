78d1698119e500e95e6c6f05429f35840895a6f4
Andrew Pepper <117658946+cillibeanThe1st@users.noreply.github.com>
2025-04-04 11:29:09 +0100

    Week 10.2

--- Week 10 - GUI.py
+++ Week 10 - GUI.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 import warnings
+
 warnings.filterwarnings("ignore")
 
 # get data
@@ -16,7 +17,7 @@ x = data.drop(['Outcome'], axis=1)
 y = data['Outcome']
 
 # split the data
-x_train, x_test, y_train, y_test = train_test_split(x,y)
+x_train, x_test, y_train, y_test = train_test_split(x, y)
 
 # scale the data
 scaler = StandardScaler()
@@ -29,3 +30,19 @@ model.fit(x_train, y_train)
 print("Model Accuracy: on training set:", model.score(x_train, y_train))
 print("Model Accuracy: on test set:", model.score(x_test, y_test))
 
+# create the function for gradio
+print(data.columns)
+
+
+def diabetes(pregnancies, glucose, blood_pressure, skinthickness, insulin, bmi, diabetespedigreefunction, age):
+    x = np.array([pregnancies, glucose, blood_pressure, skinthickness, insulin, bmi, diabetespedigreefunction, age])
+    prediction = model.predict(x.reshape(1, -1))
+    return prediction
+
+
+outputs = gr.Textbox()
+app = gr.Interface(fn=diabetes,
+                   inputs=['number', 'number', 'number', 'number', 'number', 'number', 'number', 'number'],
+                   outputs=outputs, description="This is a diabetes model")
+
+app.launch(share=True)
